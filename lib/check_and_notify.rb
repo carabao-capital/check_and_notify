7145a73ee931a32e0a5dd84865d0da109effccef
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2017-05-24 08:58:43 +0800

    Install necessary gems

--- lib/check_and_notify.rb
+++ lib/check_and_notify.rb
@@ -1,3 +1,7 @@
+require "active_support"
+require "httparty"
+require "sidekiq"
+
 require "check_and_notify/version"
 
 module CheckAndNotify
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,8 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'check_and_notify'
+require "check_and_notify"
+require "pry-nav"
+require "sidekiq/testing"
+
+Sidekiq::Testing.inline!
+
+
