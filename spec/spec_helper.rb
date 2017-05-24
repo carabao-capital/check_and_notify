283c2c1c31d6adcf0a6518359058aa32a1991b42
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2017-05-24 11:19:41 +0800

    Reset CheckAndNotify::Callbacks.check_after_one_hour_callbacks for every spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,4 +5,12 @@ require "sidekiq/testing"
 
 Sidekiq::Testing.inline!
 
+def clear_check_after_one_hour_callbacks
+  CheckAndNotify::Callbacks.check_after_one_hour_callbacks = []
+end
 
+RSpec.configure do |config|
+  config.before(:each) do
+    clear_check_after_one_hour_callbacks
+  end
+end
